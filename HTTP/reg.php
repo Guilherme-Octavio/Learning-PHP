3e117f2abc2d24852470f71674fbd0769827090c
Guilherme Octávio <guilhermeoctavio01@gmail.com>
2022-03-18 11:08:55 -0300

    sim

--- HTTP/reg.php
+++ HTTP/reg.php
@@ -46,8 +46,18 @@ if(isset($_POST['sub'])){
                         city
                         <select name="city">
                             <option value="">-select-</option>
-                            <option value="knp">kanpur</option>
-                            <option value="lko">lucknow</option>
+                           <?php
+                           $sqlcity = mysqli_query($con, "select * from city");
+
+                           while($item = mysqli_fetch_assoc($sqlcity)){
+                               $nomeItem = utf8_encode($item['nameCity']);
+                               $idCity = $item['idCity'];
+                               echo "
+                               <option value=$nomeItem>$nomeItem</option>
+                               "
+                           }
+                           
+                           ?>
                     </td>
                 </tr>
                 <tr>
